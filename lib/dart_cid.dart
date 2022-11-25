938b12fc272f3e35576afe159b3f697570b54fe1
Luís Arteiro <lucho14garfield@gmail.com>
2022-11-25 23:26:00 +0000

    feat: Basic implementation of create cid.
    Using only base58 and the raw codec.

--- lib/dart_cid.dart
+++ lib/dart_cid.dart
@@ -1,5 +1,30 @@
 library dart_cid;
 
+import 'dart:typed_data';
+
+import 'package:crypto/crypto.dart';
+import 'dart:convert';
+
+import 'package:dart_cid/multihash/dart_multihash.dart';
+import 'package:flutter/foundation.dart';
+
+import 'package:base32/base32.dart';
+import 'package:bs58/bs58.dart';
+
 String create_cid(String text) {
-  return 'bruh';
+  List<int> bytes = utf8.encode(text);
+  var digest = Uint8List.fromList(sha256.convert(bytes).bytes);
+
+  var multihash = Multihash().encode("sha2-256", digest, null);
+
+  var b = BytesBuilder();
+  b.add(Uint8List.fromList([0x01])); // adds version of cid (cidv1)
+  b.add(Uint8List.fromList([0x55])); // adds 'raw' codec to the multihash
+  b.add(multihash);
+  var hash_with_codec_and_version = b.toBytes();
+
+  var encoded = base58.encode(hash_with_codec_and_version);
+
+  var ret = "z" + encoded; // adding suffix for base 58 z
+  return ret;
 }
--- test/dart_cid_test.dart
+++ test/dart_cid_test.dart
@@ -3,5 +3,8 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:dart_cid/dart_cid.dart';
 
 void main() {
-
+  test('test', () {
+    final output = create_cid('hello world');
+    print(output);
+  });
 }
