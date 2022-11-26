af95971c12eea68f989b2d190f0a49b31d2c45d0
Luís Arteiro <lucho14garfield@gmail.com>
2022-11-26 00:16:31 +0000

    feat: Adding base encodings, documentation and basic test.

--- lib/dart_cid.dart
+++ lib/dart_cid.dart
@@ -1,5 +1,6 @@
 library dart_cid;
 
+import 'dart:ffi';
 import 'dart:typed_data';
 
 import 'package:crypto/crypto.dart';
@@ -11,20 +12,77 @@ import 'package:flutter/foundation.dart';
 import 'package:base32/base32.dart';
 import 'package:bs58/bs58.dart';
 
-String create_cid(String text) {
-  List<int> bytes = utf8.encode(text);
-  var digest = Uint8List.fromList(sha256.convert(bytes).bytes);
+enum BASE { base32, base58 }
 
-  var multihash = Multihash().encode("sha2-256", digest, null);
+/// Creates a [`cid`](https://docs.ipfs.tech/concepts/content-addressing/#what-is-a-cid) v1
+/// that is compliant with IPFS standards
+/// The returned `cid` consists of a `sha2-256` multihash, a `raw` multicodec and base encoded either 32 or 58.
+String createCid(String text, BASE base) {
+  Uint8List multihash = _createMultihash(text);
+  Uint8List suffixedMultihash = _addSuffixToMultihash(multihash);
 
-  var b = BytesBuilder();
+  if (base == BASE.base32) {
+    return _base32Encode(suffixedMultihash);
+  } 
+  else if (base == BASE.base58) {
+    return _base58Encode(suffixedMultihash);
+  } 
+  else {
+    throw UnsupportedError('Only \'base32\' and \'base58\' are supported.');
+  }
+}
+
+/// Adds a suffix to the multihash.
+///
+/// Currently, the suffix consists of `CIDv1` **version code** and `raw` multicodec.
+/// These are part of the `CIDv1` hash. For more information about the information
+/// that is present inside a `CIDv1` hash, check the [official IPFS](https://docs.ipfs.tech/concepts/content-addressing/#cid-versions) docs.
+Uint8List _addSuffixToMultihash(Uint8List multihash) {
+  BytesBuilder b = BytesBuilder();
+
+  // Adds the convention codes for the version and the codec.
+  // These codes can be found in the canonical repo -> https://github.com/multiformats/multicodec/blob/master/table.csv
   b.add(Uint8List.fromList([0x01])); // adds version of cid (cidv1)
   b.add(Uint8List.fromList([0x55])); // adds 'raw' codec to the multihash
   b.add(multihash);
-  var hash_with_codec_and_version = b.toBytes();
 
-  var encoded = base58.encode(hash_with_codec_and_version);
+  return b.toBytes();
+}
 
-  var ret = "z" + encoded; // adding suffix for base 58 z
-  return ret;
+/// Creates a [Multihash](https://multiformats.io/multihash/) hashed string
+/// with the given input.
+///
+/// It hashes the input string with `sha2-256` and suffixes it with the
+/// correspondent Multiformat convention code.
+Uint8List _createMultihash(String text) {
+  List<int> bytes = utf8.encode(text);
+
+  // Hash the input string with sha2-256 algorithm and  convert it to array of bytes
+  Digest digestObj = sha256.convert(bytes);
+  Uint8List digest = Uint8List.fromList(digestObj.bytes);
+
+  // Multihashes the hashed string with the `sha2-256` multiformat code.
+  Uint8List multihash = Multihash().encode("sha2-256", digest, null);
+
+  return multihash;
+}
+
+/// Base58 encodes the given [suffixedMultihash].
+/// It appends the 'z' character to the string, according to the
+/// [`multibase` table](https://github.com/multiformats/multibase#multibase-table)
+String _base58Encode(Uint8List suffixedMultihash) {
+  String encodedHash = base58.encode(suffixedMultihash);
+
+  // Suffixing the encoded hash with 'z'
+  return "z$encodedHash";
+}
+
+/// Base32 encodes the given [suffixedMultihash].
+/// It appends the 'b' character to the string, according to the
+/// [`multibase` table](https://github.com/multiformats/multibase#multibase-table)
+String _base32Encode(Uint8List suffixedMultihash) {
+  String encodedHash = base32.encode(suffixedMultihash);
+
+  // Suffixing the encoded hash with 'z'
+  return "b$encodedHash";
 }
--- test/dart_cid_test.dart
+++ test/dart_cid_test.dart
@@ -3,8 +3,11 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:dart_cid/dart_cid.dart';
 
 void main() {
-  test('test', () {
-    final output = create_cid('hello world');
-    print(output);
+  test('test creating cid with \'hello world \' with base58 encoding', () {
+    String input = 'hello world';
+    final output = createCid(input, BASE.base58);
+
+    // https://cid.ipfs.tech/#zb2rhj7crUKTQYRGCRATFaQ6YFLTde2YzdqbbhAASkL9uRDXn
+    expect(output == "zb2rhj7crUKTQYRGCRATFaQ6YFLTde2YzdqbbhAASkL9uRDXn", true);
   });
 }
